56d4da1b2c01a1ce26c386bdac4dd70b6f2b5b16
Alex Dahl <hello@alexdahl.com>
2012-07-08 22:13:04 -0700

    initial css graphics

--- Oversight.wdgt/core.js
+++ Oversight.wdgt/core.js
@@ -200,13 +200,11 @@ function switchTab(tab){ // called when the user presses a tab
 	if(tab == 0){ // switch to CPU
 		document.getElementById('ram').className = "tabInactive";
 		document.getElementById('cpu').className = "tabActive";
-		document.getElementById('tabs').style.backgroundImage = "url(Images/tab_l.png)";
 		psCommand = "/bin/ps -racwwxo \"%cpu comm\" | head -6 | tail -5";
 		widget.setPreferenceForKey(false, "tab");
 	}else{ // switch to RAM
 		document.getElementById('cpu').className = "tabInactive";
 		document.getElementById('ram').className = "tabActive";
-		document.getElementById('tabs').style.backgroundImage = "url(Images/tab_r.png)";
 		psCommand = "/bin/ps -macwwxo \"rss comm\" | head -6 | tail -5";
 		widget.setPreferenceForKey(true, "tab");
 	}
